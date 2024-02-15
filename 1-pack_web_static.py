46eaf6091486771e38ea521eacee2e703bc5b7c1
uwituzeb <b.uwituze@alustudent.com>
2024-02-15 18:00:07 +0200

    fix errors

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """
-deploy archive
+This is module 1-pack_web_static.py
+The following is a fabfile
 """
 
-from fabric.api import *
+from fabric.api import local
 from datetime import datetime
 
-
+@task
 def do_pack():
+  """
+    a Fabric script that generates a .tgz archive from the contents of the
+    web_static folder of your AirBnB Clone repo, using the function do_pack.
+  """
   timestr = datetime.strftime("%Y%m%d%H%M%S")
   try:
     local("mkdir -p versions")
