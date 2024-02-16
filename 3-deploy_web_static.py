dd66b66cb9154f9b3890a68ce1a95376aca2a1f3
uwituzeb <b.uwituze@alustudent.com>
2024-02-16 13:00:11 +0200

    deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,14 +3,14 @@
 This is a Fabric script to create and distribute an archive to web servers using the deploy function.
 """
 
-from fabric.api import task
+from fabric.api import task, env, run, put
 from fabric.state import output
 from fabric.colors import green, red
 from os.path import isfile
-from fabric.operations import local
 
-from 1-pack_web_static import do_pack
-from 2-do_deploy_web_static import do_deploy
+# Importing modules with modified names
+import pack_web_static
+import do_deploy_web_static
 
 output['running'] = False
 output['warnings'] = False
@@ -26,7 +26,7 @@ def deploy():
         bool: True if deployment is successful, False otherwise.
     """
     # Call the do_pack function and store the path of the created archive
-    archive_path = do_pack()
+    archive_path = pack_web_static.do_pack()
 
     if not archive_path or not isfile(archive_path):
         print(red("Failed to create archive. Deployment aborted."))
@@ -35,7 +35,7 @@ def deploy():
     print(green("Archive created: {}".format(archive_path)))
 
     # Call the do_deploy function using the new path of the new archive
-    deployment_result = do_deploy(archive_path)
+    deployment_result = do_deploy_web_static.do_deploy(archive_path)
 
     if deployment_result:
         print(green("Deployment successful!"))
@@ -45,4 +45,9 @@ def deploy():
         return False
 
 if __name__ == "__main__":
+    # Set your server IP addresses, username, and private key
+    env.hosts = ['<IP web-01>', '<IP web-02>']
+    env.user = '<your_username>'
+    env.key_filename = '<path_to_your_private_key>'
+
     deploy()
