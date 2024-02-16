7acdd8c31d27d9a671955d9d5f896bb2f22b3437
Eliane_M <munezeroeliane761@gmail.com>
2024-02-16 02:06:01 -0800

    archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,25 @@
+#!usr/bin/env bash
+# generates an archive out of web-static folder
+
+from fabric import task as fabric_task
+from fabric.operations import local
+from datetime import datetime
+
+@fabric_task
+def do_pack(c):
+    """
+    Creates a .tgz archive from the contents of the web_static folder.
+
+    Returns:
+        (str): Archive path if successfully generated, None otherwise.
+    """
+    try:
+        now = datetime.now()
+        timestamp = now.strftime("%Y%m%d%H%M%S")
+        archive_name = "web_static_{}.tgz".format(timestamp)
+        local("mkdir -p versions")
+        local("tar -czvf versions/{} web_static".format(archive_name))
+        return "versions/{}".format(archive_name)
+    except Exception as e:
+        print("Error occurred while creating archive:", e)
+        return None
