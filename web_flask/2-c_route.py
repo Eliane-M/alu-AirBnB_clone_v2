00d14fe10c344e8ddd8978dbbede1eba5519e38b
Eliane_M <munezeroeliane761@gmail.com>
2024-02-22 11:20:01 -0800

    routes

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """
+    flask hello world
+    """
+    return "Hello HBNB!"
+    strict_slashes = False
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    displays HBNB
+    """
+    return "HBNB"
+    strict_slashes = False
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """
+    make a simple variable rule
+    """
+    return "C {}".format(text.replace("_", " "))
+    strict_slashes = False
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
