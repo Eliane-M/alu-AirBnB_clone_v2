68862b177737eba2567191ec7cb9b1b60e8b5ecc
Eliane-M <125857083+Eliane-M@users.noreply.github.com>
2024-02-23 16:49:53 +0200

    routes

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,46 +1,41 @@
 #!/usr/bin/python3
 """
-This script starts a Flask web application
+script starts Flask web app
+listen on 0.0.0.0, port 5000
 """
 
-
 from flask import Flask
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route("/")
 def hello_hbnb():
-    """
-    flask hello world
-    """
+    """display text"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb')
+@app.route("/hbnb")
 def hbnb():
-    """
-    displays HBNB
-    """
+    """display text"""
     return "HBNB"
 
 
-@app.route('/c/<text>')
+@app.route("/c/<text>")
 def c_text(text):
-    """
-    make a simple variable rule
-    """
+    """display custom text given"""
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/(<text>)')
-def python_text(text='is cool'):
+@app.route("/python")
+@app.route("/python/<text>")
+def python_text(text="is cool"):
     """
-    python followed by a text
+    display custom text given
     """
     return "Python {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
